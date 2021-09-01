e9912c51f0ac12acdfadb78781ed4953b5fa6d60
Aloxan12 <kvakvaaak@mail.ru>
2021-09-01 14:53:54 +0300

    add todo

--- App.js
+++ App.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Navbar} from "./src/Navbar";
 import {AddTodo} from "./src/AddTodo";
+import {Todo} from "./src/Todo";
 
 
 export default function App() {
+    const [todos, setTodos] = useState([])
 
-  return (
-    <View>
-      <Navbar title={'Todos App'} />
-      <View style={styles.container}>
-          <AddTodo />
-      </View>
-    </View>
-  );
+    const addTodo=(title)=>{
+        const newTodo = {
+            id: new Date().toString(),
+            title: title
+        }
+        setTodos(prev => [...prev, newTodo])
+    }
+
+    return (
+        <View>
+            <Navbar title={'Todos App'}/>
+            <View style={styles.container}>
+                <AddTodo onSubmit={addTodo}/>
+                <View>
+                    {todos.map(t => <Todo todo={t} key={t.id} />)}
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-      paddingHorizontal: 30,
-      paddingVertical: 20
-  },
+    container: {
+        paddingHorizontal: 30,
+        paddingVertical: 20
+    },
 });
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, StyleSheet, TextInput, View} from 'react-native';
 
 
-export const AddTodo =()=> {
-    const onPressHandler =()=>{
+export const AddTodo =({onSubmit})=> {
+    const [value, setValue] = useState('')
 
+    const onPressHandler =()=>{
+        onSubmit(value)
+        setValue('')
     }
     return (
         <View style={styles.block}>
-            <TextInput style={styles.input} />
-            <Button title={'Добавить'} onPress={onPressHandler} />
+            <TextInput
+                style={styles.input}
+                onChangeText={setValue}
+                value={value}
+                placeholder='Введите название дела...'
+            />
+            <Button title={'Добавить'} onPress={()=>onPressHandler(value)} />
         </View>
     );
 }
